0b40c211996cb5c124bace5e27804f3dd3d5829e
Katie Saramutina <saramutina@gmail.com>
2023-06-20 21:08:07 +0400

    Initial Commit

---
+++ AskAway/AskAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskAwayApp.swift
+//  AskAway
+//
+//  Created by Katie Saramutina on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AskAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
